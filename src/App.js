96f309cd88cc3ffa8a60a7597135cab576af6cda
aNesterovic <anest1995@ukr.net>
2022-03-29 21:10:27 +0300

    add ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Navbar from './Navbar';
-import Home from './Home'
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="content">
-        <Home/>
+        <Home />
       </div>
     </div>
   );
