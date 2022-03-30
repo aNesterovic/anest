b631b6279582160485df64ea3304bba8f555ef15
aNesterovic <anest1995@ukr.net>
2022-03-30 13:37:52 +0300

    add props for blockList and creat seperate component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogsList from './BlogsList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -14,12 +15,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogsList blogs={blogs} title="All block list" />
     </div>
   );
 };
