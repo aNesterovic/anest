a47aeb0b31a3f5c38551c66298075104dd659e9a
aNesterovic <anest1995@ukr.net>
2022-03-30 16:26:52 +0300

    add loading message while fetching data

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,22 @@ import BlogsList from './BlogsList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState('mario');
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((resp) => resp.json())
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((resp) => resp.json())
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogsList blogs={blogs} title="All block list" />}
     </div>
   );
