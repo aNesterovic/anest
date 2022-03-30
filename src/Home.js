bbc500f394568badf0fa3d2c0b7e970240228730
aNesterovic <anest1995@ukr.net>
2022-03-30 16:51:53 +0300

    add handler of error message

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,33 @@ import BlogsList from './BlogsList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
-        .then((resp) => resp.json())
+        .then((resp) => {
+          console.log(resp);
+          if (!resp.ok) {
+            throw Error('could not fretch the data for that resource');
+          }
+          return resp.json();
+        })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogsList blogs={blogs} title="All block list" />}
     </div>
